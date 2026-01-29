dd455a3a025033c285b1f5a5986c6e7125d17637
Afzaal Web <afzaalwebsolutions@gmail.com>
2026-01-29 17:06:00 +0500

    updated script

--- 05 Test Scripts/creat_customer.sql
+++ 05 Test Scripts/creat_customer.sql
@@ -1,12 +1,12 @@
 -- ===================== Step 1: Prepare JSON request =====================
 SET @pjReqObj = JSON_OBJECT(
-  'customer_rec_id', 2,
+--  'customer_rec_id', 2,
   'first_name', 'ASAD',
   'last_name', 'Khan',
-  'email', 'aize.khan@finapp.com',
+  'email', 'asad.khan@finapp.com',
   'phone', '+971501234567',
   'whatsapp_number', '+971501234567',
-  'national_id', 'UAE78419901',
+  'national_id', '4546DFGRT',
   'password', 'Secure@789'
 );
 
