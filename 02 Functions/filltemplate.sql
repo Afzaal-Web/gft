dd8d48c1f17c8236071ff8c46ea6b00f5c2c95bb
Etech-afzaal <etech.afzaal@gmail.com>
2026-03-10 14:04:42 +0500

    some changes in filltemplate

--- 02 Functions/filltemplate.sql
+++ 02 Functions/filltemplate.sql
@@ -15,6 +15,7 @@ BEGIN
     DECLARE v_result 			JSON DEFAULT pjTemplateObj;
     DECLARE v_req_keys 			JSON;
     DECLARE v_key 				VARCHAR(255);
+    DECLARE v_clean_key     	VARCHAR(255);
     DECLARE v_value 			JSON;
     DECLARE i 					INT DEFAULT 0;
     DECLARE v_count 			INT;
@@ -24,10 +25,22 @@ BEGIN
     SET v_count 		= JSON_LENGTH(v_req_keys);
 
     WHILE i < v_count DO
+		 -- Original key (example: P_DOC_TYPE)
         SET v_key 		= JSON_UNQUOTE(JSON_EXTRACT(v_req_keys, CONCAT('$[', i, ']')));
+         -- Extract value
         SET v_value 	= JSON_EXTRACT(pjReqObj, CONCAT('$.', v_key));
+        
+        -- Remove leading 'P_' only if present
+        IF LEFT(v_key, 2) = 'P_' THEN
+            SET v_clean_key = SUBSTRING(v_key, 3);
+        ELSE
+            SET v_clean_key = v_key;
+        END IF;
+        
+        -- Convert to lowercase
+        SET v_clean_key = LOWER(v_clean_key);
 
-        SET v_result = 	  buildJSONSmart(v_result, v_key, v_value);
+        SET v_result = buildJSONSmart(v_result, v_clean_key, v_value);
 
         SET i = i + 1;
     END WHILE;
