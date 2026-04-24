f3a21ac795b7be3a681c9068079284381e5eb0b6
Etech-afzaal <etech.afzaal@gmail.com>
2026-04-24 11:56:34 +0500

    pjRespObj

--- 03 Stored Procedure/14_resetPassword.sql
+++ 03 Stored Procedure/14_resetPassword.sql
@@ -7,8 +7,8 @@ DROP PROCEDURE IF EXISTS resetPassword;
 DELIMITER $$
 
 CREATE PROCEDURE resetPassword(
-                                IN  pjReqObj       JSON,
-                                OUT pResJson      JSON
+                                IN    pjReqObj    JSON,
+                                INOUT pjRespObj   JSON
                               )
 BEGIN
 
@@ -21,7 +21,18 @@ BEGIN
     DECLARE v_email               VARCHAR(255);
     DECLARE v_phone               VARCHAR(20);
     DECLARE v_token_expiry        DATETIME;
-    DECLARE v_db_reset_token 	 VARCHAR(100);
+    DECLARE v_db_reset_token 	  VARCHAR(100);
+
+    DECLARE v_err_msg           TEXT;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET STACKED DIAGNOSTICS CONDITION 1 v_err_msg = MESSAGE_TEXT;
+
+        SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+        SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', CONCAT('Unexpected Error - ', v_err_msg));
+    END;
+
 
     main_block: BEGIN
 
@@ -34,10 +45,10 @@ BEGIN
 
         -- Validate input
         IF v_login_id IS NULL OR v_new_password IS NULL OR v_reset_token IS NULL THEN
-            SET pResJson = JSON_OBJECT(
-										'status',  'INVALID_REQUEST',
-										'message', 'Login ID, New Password and Reset Token are required.'
-									);
+
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'Login ID, New Password and Reset Token are required.');
+
             LEAVE main_block;
         END IF;
 
@@ -54,28 +65,28 @@ BEGIN
 
         -- Customer not found
         IF v_customer_rec_id IS NULL THEN
-            SET pResJson = JSON_OBJECT(
-										'status',  'USER_NOT_FOUND',
-										'message', 'No account matches the provided login ID.'
-									);
+
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'No account matches the provided login ID.');
+
             LEAVE main_block;
         END IF;
 
         -- Token mismatch
         IF v_reset_token <>  v_db_reset_token THEN
-            SET pResJson = JSON_OBJECT(
-										'status', 'INVALID_TOKEN',
-										'message', 'The reset token is invalid.'
-									);
+
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'The reset token is invalid.');
+
             LEAVE main_block;
         END IF;
 
         -- Token expired
         IF NOW() > v_token_expiry THEN
-            SET pResJson = JSON_OBJECT(
-										'status', 'EXPIRED_TOKEN',
-										'message', 'The reset token has expired.'
-									);
+
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'The reset token has expired.');
+
             LEAVE main_block;
         END IF;
 
@@ -84,15 +95,15 @@ BEGIN
         -- =========================
         IF EXISTS (
             SELECT 1 
-            FROM password_history
+            FROM    password_history
             WHERE 	parent_table_name = 'customer'
             AND		parent_table_rec_id = v_customer_rec_id
 			AND 	password_hash = SHA2(v_new_password,256)
         ) THEN
-            SET pResJson = JSON_OBJECT(
-										'status',  'PASSWORD_REUSED',
-										'message', 'You cannot reuse a previous password.'
-									);
+
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 1);
+            SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'You cannot reuse a previous password.');
+
             LEAVE main_block;
         END IF;
 
@@ -121,7 +132,7 @@ BEGIN
               parent_table_rec_id		= v_customer_rec_id,
               password_hash				= SHA2(v_new_password,256),
               password_set_at			= NOW(),
-              password_change_by		= 'customer',
+              password_changed_by		= 'customer',
               password_change_reason	= 'forgot password',
               last_password_updated_at  = NOW(),
               password_expiration_date	= DATE_ADD(NOW(), INTERVAL 90 DAY),
@@ -137,10 +148,9 @@ BEGIN
         -- =========================
         -- Success Response
         -- =========================
-        SET pResJson = JSON_OBJECT(
-            'status',  'PASSWORD_RESET_SUCCESS',
-            'message', 'Your password has been successfully reset.'
-        );
+
+        SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.responseCode', 0);
+        SET pjRespObj = buildJSONSmart(pjRespObj, 'jHeader.message', 'Your password has been successfully reset.');
 
     END main_block;
 
